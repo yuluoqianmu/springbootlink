631a71713f08c62f7e2f8aca31fa3f49e37abb0d
shaomaolin <350083890@qq.com>
2019-08-04 07:18:45 +0800

    zookeeper分布式锁

---
+++ link-zookeeper/src/main/java/com/maolin/zookeeper/LinkZookeeperApplication.java
@@ -0,0 +1,13 @@
+package com.maolin.zookeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkZookeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkZookeeperApplication.class, args);
+    }
+
+}
